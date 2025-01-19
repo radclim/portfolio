ba19d509fd89b8fd7700a76fa6ef14664aacbaa7
radclim <mmajaaaa@gmail.com>
2025-01-19 14:28:35 +0100

    logo

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,13 +14,13 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "An opinionated starter theme for Astro",
+	description: "Maja Miskeri's Web Developer Portfolio",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "en_GB",
 	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-	title: "Astro Theme Cactus",
+	title: "Maja Miskeri's Portfolio",
 };
 
 // Used to generate links in both the Header & Footer.
